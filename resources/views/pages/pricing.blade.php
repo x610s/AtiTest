66fad552c55c9e2813f76f47779dbee7b1f0b110
unknown <jmdnbvsjm@gmail.com>
2021-03-22 10:01:58 -0400

    cambios

--- resources/views/pages/pricing.blade.php
+++ resources/views/pages/pricing.blade.php
@@ -2,5 +2,23 @@
 
 
 @section('contenido')
-    
+    <div class="container my-5">
+        <div class="row">
+            <div class="col-3">
+                <div class="card">
+                    <img class="card-img-top" src="holder.js/100x180/" alt="">
+                    <div class="card-body">
+                        <h4 class="card-title">Title</h4>
+                        <p class="card-text">Text</p>
+
+                        <div>
+                        
+                            <a class="btn btn-outline-primary btn-block">
+                                <i class="fa fa-cart-plus" aria-hidden="true"></i> Comprar</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AdminDashboardController;
+use App\Http\Controllers\PricinController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
@@ -11,6 +12,7 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 Route::resource('usuario', UserController::class);
 Route::resource('admin', AdminDashboardController::class);
+Route::resource('pricing', PricinController::class);
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 
